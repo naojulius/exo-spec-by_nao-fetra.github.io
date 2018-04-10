04a935b50a5072ec7144e7eb70da9a7f7e9a4daa
fetra <harinjatovo.fetra@gmail.com>
2018-04-10 14:44:20 +0300

    book

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,7 +1,17 @@
 class Book
 # write your code here
-
+    def title=(title)
+        mots = title.split(" ") 
+        for i in 0..(mots.length - 1) do
+            if i == 0
+                mots[i].capitalize!
+            elsif mots[i] != "the" && mots[i] != "a" && mots[i] != "an" && mots[i] != "and" && mots[i] != "of" && mots[i] != "of"
+                mots[i].capitalize!
+            end
+        end 
+       @title = mots.join(" ")
+    end
+    def title
+        @title
+    end
 end
-
-
-
